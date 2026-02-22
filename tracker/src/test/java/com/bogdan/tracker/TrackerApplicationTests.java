62d765ff0f458c5663b0846ba1e16e3e5641b73b
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2026-02-22 11:12:42 +0300

    init commit

---
+++ tracker/src/test/java/com/bogdan/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdan.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
